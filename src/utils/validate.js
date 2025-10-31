389b5b4a57a09b1e28e99d533b7152c71adca965
Mohd Atfan <143632074+atfan7@users.noreply.github.com>
2025-10-31 21:20:26 +0530

    Refactor checkValidateData for email and password validation
    Refactored checkValidateData function to simplify validation rules for email and password.

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,18 +1,8 @@
+  export const checkValidateData = (email, password) => {
+  const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
 
+  if (!isEmailValid) return "Email ID is not valid";
+  if (password.length < 6) return "Password must be at least 6 characters";
 
-export const checkValidateData =(email,password)=>{
-
-    const isEmailValid =/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email) ;
-    const isPasswordValid =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
-
-    if(!isEmailValid) return "Email ID is not valid ";
-    if(!isPasswordValid) return "Password is not valid"
-
-    return null;
-
-
-
-
-
-
-}
+  return null;
+};
